0416376a8d831502ee78f582d0ced20d38da220d
AleksaLab <116030291+AleksaLab@users.noreply.github.com>
2023-03-13 18:59:32 +0100

    Add files via upload

---
+++ main (1).cs
@@ -0,0 +1,30 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Goodbye World");
+  {
+    byte b = 1;
+    short sh = 11;
+    int yntydżer = 111;
+    long takeThatL = 1111;
+    float plynac = 1.1F;
+    double podwojny = 11111;
+    char czaryMary = 'A';
+    bool bulbulbul = true;
+    decimal d = 1.11M;
+    Console.WriteLine($"char = {czaryMary}");
+    Console.WriteLine($"int = {yntydżer}");
+    Console.WriteLine($"byte = {b}");
+    Console.WriteLine($"short = {sh}");
+    Console.WriteLine($"double = {podwojny}");
+    Console.WriteLine($"long = {takeThatL}");
+    Console.WriteLine($"float = {plynac}");
+    Console.WriteLine($"bool = { bulbulbul}");
+    Console.WriteLine($"decimal = {d}");
+    
+    
+    Console.ReadLine();
+  }
+  }
+}
